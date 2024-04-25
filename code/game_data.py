1c3dfff1a09c4e92ae29ce06aac60e8cc947ad40
Sajawal Nauman <sajawal.nauman@mail.utoronto.ca>
2024-04-25 18:29:22 -0400

    Final Release

---
+++ code/game_data.py
@@ -0,0 +1,60 @@
+level_0 = {
+    'node_pos': (145, 155),
+    'node_graphic': 'graphics/overworld/level__0.png',
+    'terrain': 'levels/0/level_0_terrain.csv',
+    'jump_pads': 'levels/0/level_0_jump_pads.csv',
+    'player': 'levels/0/level_0_player.csv',
+    'coins': 'levels/0/level_0_coins.csv',
+    'keys': 'levels/0/level_0_keys.csv',
+    'torches': 'levels/0/level_0_torches.csv',
+    'spikes': 'levels/0/level_0_spikes.csv',
+    'constraints': 'levels/0/level_0_constraints.csv',
+    'unlock': 1,
+}
+level_1 = {
+    'node_pos': (355, 155),
+    'node_graphic': 'graphics/overworld/level__1.png',
+    'terrain': 'levels/1/level_1_terrain.csv',
+    'jump_pads': 'levels/1/level_1_jump_pads.csv',
+    'player': 'levels/1/level_1_player.csv',
+    'coins': 'levels/1/level_1_coins.csv',
+    'keys': 'levels/1/level_1_keys.csv',
+    'torches': 'levels/1/level_1_torches.csv',
+    'spikes': 'levels/1/level_1_spikes.csv',
+    'constraints': 'levels/1/level_1_constraints.csv',
+    'unlock': 2,
+}
+level_2 = {
+    'node_pos': (145, 264),
+    'node_graphic': 'graphics/overworld/level__2.png',
+    'terrain': 'levels/2/level_2_terrain.csv',
+    'jump_pads': 'levels/2/level_2_jump_pads.csv',
+    'player': 'levels/2/level_2_player.csv',
+    'coins': 'levels/2/level_2_coins.csv',
+    'keys': 'levels/2/level_2_keys.csv',
+    'torches': 'levels/2/level_2_torches.csv',
+    'spikes': 'levels/2/level_2_spikes.csv',
+    'constraints': 'levels/2/level_2_constraints.csv',
+    'unlock': 3,
+}
+level_3 = {
+    'node_pos': (355, 264),
+    'node_graphic': 'graphics/overworld/level__3.png',
+    'terrain': 'levels/0/level_0_terrain.csv',
+    'jump_pads': 'levels/0/level_0_jump_pads.csv',
+    'player': 'levels/0/level_0_player.csv',
+    'coins': 'levels/0/level_0_coins.csv',
+    'keys': 'levels/0/level_0_keys.csv',
+    'torches': 'levels/0/level_0_torches.csv',
+    'spikes': 'levels/0/level_0_spikes.csv',
+    'constraints': 'levels/0/level_0_constraints.csv',
+    'unlock': 4,
+}
+
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    # 3: level_3,
+}
